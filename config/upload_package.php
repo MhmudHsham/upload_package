e18b64b411716c8000e98c210842e121405c1423
Mahmoud Gabr <mhmudhsham8@gmail.com>
2019-06-07 03:23:58 +0400

    add code

--- config/upload_package.php
+++ config/upload_package.php
@@ -1,5 +1,17 @@
 <?php
 
 return [
-    //
+
+	"messages" => [
+		"invalid_extension" => "Not allowed extension.",
+		"invalid_size" => "Not allowed size.",
+	],
+
+	// Copy this array when creating
+	"slider_image" => [
+		"file_extension" => ["jpg", "jpeg", "png"],
+		"file_size" => 100000,
+		"folder_name" => "sliders",
+	],
+
 ];
--- src/upload_package.php
+++ src/upload_package.php
@@ -2,7 +2,26 @@
 
 namespace mhmudhsham\upload_package;
 
-class upload_package
-{
-    // Build wonderful things
+class upload_package {
+	public function upload_file($file, $config) {
+		$file_uploaded_extension = $file->getClientOriginalExtension();
+
+		$allowed_extensions = config("upload.$config.file_extension");
+
+		if (!in_array($file_uploaded_extension, $allowed_extensions)) {
+			return [false, config("upload.messages.invalid_extension")];
+		}
+
+		$allowed_size = config("upload.$config.file_size");
+
+		$file_uploaded_size = $file->getSize();
+
+		if ($file_uploaded_size > $allowed_size) {
+			return [false, config("upload.messages.invalid_size")];
+		}
+
+		$uploaded_file = $file->store('public/' . config("upload.$config.folder_name"));
+
+		return [true, $uploaded_file];
+	}
 }
