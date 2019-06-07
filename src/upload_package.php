f304e129acf992a7fb1704c4bf29480260659b49
Mahmoud Gabr <mhmudhsham8@gmail.com>
2019-06-07 18:03:36 +0400

    edit in config file title

--- src/upload_package.php
+++ src/upload_package.php
@@ -6,21 +6,21 @@ class upload_package {
 	public function upload_file($file, $config) {
 		$file_uploaded_extension = $file->getClientOriginalExtension();
 
-		$allowed_extensions = config("upload.$config.file_extension");
+		$allowed_extensions = config("upload_package.$config.file_extension");
 
 		if (!in_array($file_uploaded_extension, $allowed_extensions)) {
-			return [false, config("upload.messages.invalid_extension")];
+			return [false, config("upload_package.messages.invalid_extension")];
 		}
 
-		$allowed_size = config("upload.$config.file_size");
+		$allowed_size = config("upload_package.$config.file_size");
 
 		$file_uploaded_size = $file->getSize();
 
 		if ($file_uploaded_size > $allowed_size) {
-			return [false, config("upload.messages.invalid_size")];
+			return [false, config("upload_package.messages.invalid_size")];
 		}
 
-		$uploaded_file = $file->store('public/' . config("upload.$config.folder_name"));
+		$uploaded_file = $file->store('public/' . config("upload_package.$config.folder_name"));
 
 		return [true, $uploaded_file];
 	}
